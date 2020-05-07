03a21a03383bc45e76ab6df9a05088793e0b1496
pkt0831 <pkt0831@naver.com>
2020-05-07 20:52:56 +0900

    add login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
+/* eslint-disable react/void-dom-elements-no-children */
+/* eslint-disable react/button-has-type */
 import React, { Component } from 'react';
+import classNames from 'classnames/bind';
 import style from './App.css';
 
+const st = classNames.bind(style);
+
 class App extends Component {
+  // eslint-disable-next-line class-methods-use-this
+  handleSubmitChange = () => {
+    // eslint-disable-next-line no-console
+    console.log('사용자 아이디 입력중');
+  };
+
   render() {
     return (
-      <div className={`${style.App} ${style.blue}`}>
-        <h1>Hello World</h1>
-      </div>
+      <>
+        <div>
+          <h1>로그인</h1>
+        </div>
+        <div className={st('wrap')}>
+          <form classNAme={st('formStyle')} onSubmit={this.handleSubmit}>
+            <h2>ID:</h2>
+            <input
+              type="text"
+              className={st('inputStyle')}
+              onSubmit={this.handlePw}
+            />
+            <h3>PASS</h3>
+            <input
+              type="text"
+              className={st('inputStyle')}
+              onSubmit={this.handleLogin}
+            />
+            <button className={st('buttonStyle')}>로그인하기</button>
+          </form>
+        </div>
+      </>
     );
   }
 }
