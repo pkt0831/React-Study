29b82d561328003034393b4e9cc2f6a6d3ef1a1c
pkt0831 <pkt0831@naver.com>
2020-05-08 17:30:14 +0900

    for pull

--- src/ButtonChild02.js
+++ src/ButtonChild02.js
@@ -1,30 +1,18 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/button-has-type */
 // eslint-disable-next-line no-unused-vars
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 
-// eslint-disable-next-line react/prop-types
-const ButtonChild02 = ({ title, color }) => {
-  const ButtonStyle = {
-    border: 'none',
-    width: 100,
-    height: 30,
-    background: color,
-  };
-  const handleButtonClick = () => {
-    // eslint-disable-next-line no-console
-    console.log('Button Child02 클릭!');
-  };
-  return (
-    // eslint-disable-next-line react/button-has-type
-    <button style={ButtonStyle} onClick={handleButtonClick}>
-      {title}
-    </button>
-  );
+const ButtonStyle = {
+  border: 'none',
+  width: 100,
+  height: 30,
+  background: 'yellow',
 };
 
-ButtonChild02.propTypes = {
-  // eslint-disable-next-line react/require-default-props
-  title: PropTypes.number,
-};
+const ButtonChild02 = (props) => (
+  <button style={ButtonStyle}>{props.title}</button>
+);
 
 export default ButtonChild02;
--- src/Test.js
+++ src/Test.js
@@ -43,27 +43,3 @@ const App = () => {
 };
 
 export default App;
-
-// import React, { Component } from 'react';
-// import ButtonChild01 from './ButtonChild01';
-// import ButtonChild02 from './ButtonChild02';
-
-// const AppStyle = {
-//   border: '1px solid #333',
-//   padding: 100,
-//   margin: 100,
-// };
-// class App extends Component {
-//   render() {
-//     return (
-//       // eslint-disable-next-line no-undef
-//       <div className="App" style={AppStyle}>
-//         <h2>부모 컴포넌트</h2>
-//         <ButtonChild01 title="주황색 버튼" />
-//         <ButtonChild02 title="노란색 버튼" />
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
